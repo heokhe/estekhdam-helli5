db14bb88e67c1f9ffaa47c7d1824774864498ded
Hosein Khansari <hrk.x11@gmail.com>
2023-01-01 11:24:09 +0330

    some migrations

---
+++ prisma/migrations/20221230140107_/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `imageAddres` to the `Application` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Application" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "time" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "categoryId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "email" TEXT,
+    "imageAddres" TEXT NOT NULL,
+    "cvAddress" TEXT,
+    CONSTRAINT "Application_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Application" ("categoryId", "cvAddress", "email", "id", "lastName", "name", "phoneNumber", "time") SELECT "categoryId", "cvAddress", "email", "id", "lastName", "name", "phoneNumber", "time" FROM "Application";
+DROP TABLE "Application";
+ALTER TABLE "new_Application" RENAME TO "Application";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
