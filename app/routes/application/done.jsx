f575e96e69a83d3829ea9356d25db7ddfcc2e257
Hosein Khansari <hrk.x11@gmail.com>
2023-02-08 15:24:53 +0330

    Redesign the application done page

--- app/routes/application/done.jsx
+++ app/routes/application/done.jsx
@@ -1,3 +1,32 @@
+import HandshakeOutlined from '@mui/icons-material/HandshakeOutlined'
+import OpenInNew from '@mui/icons-material/OpenInNew'
+import { Container, Link as MuiLink, Typography } from '@mui/material'
+
 export default function ApplicationDone() {
-  return <h1>ممنون!</h1>
+  return (
+    <Container maxWidth="md" sx={{ textAlign: 'center', pt: 10, pb: 5 }}>
+      <HandshakeOutlined color="primary" sx={{ fontSize: '10vw' }} />
+      <Typography variant="h3" sx={{ mb: 2, mt: 3, fontWeight: 500 }}>
+        از همکاری شما صمیمانه متشکریم!
+      </Typography>
+      <Typography variant="h5" color="text.secondary">
+        درخواست استخدام شما ثبت شد.
+        <br />
+        همکاران ما پس از بررسی با شما تماس خواهند گرفت.
+      </Typography>
+      <Container maxWidth="sm" sx={{ mt: 3 }}>
+        <MuiLink href="https://allamehelli5.ir" target="_blank">
+          وبسایت دبیرستان علامه حلی پنج
+          <OpenInNew
+            sx={{
+              verticalAlign: 'middle',
+              ml: '0.1em',
+              transform: 'scaleX(-1)',
+            }}
+            fontSize="inherit"
+          />
+        </MuiLink>
+      </Container>
+    </Container>
+  )
 }
