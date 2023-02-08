5e32f82d76076ab279d8f107525365674f0831ef
Hosein Khansari <hrk.x11@gmail.com>
2023-02-08 15:23:08 +0330

    enable responsive font sizes

--- app/theme.js
+++ app/theme.js
@@ -1,24 +1,26 @@
-import { createTheme } from '@mui/material/styles'
+import { createTheme, responsiveFontSizes } from '@mui/material/styles'
 import rtlPlugin from 'stylis-plugin-rtl'
 import createCache from '@emotion/cache'
 import { prefixer } from 'stylis'
 import { faIR as dataGridFaIR } from '@mui/x-data-grid'
 import { faIR } from '@mui/material/locale'
 
-export const theme = createTheme(
-  {
-    direction: 'rtl',
-    typography: {
-      fontFamily: 'Estedad',
-    },
-    palette: {
-      background: {
-        // default: "#fafafa",
+export const theme = responsiveFontSizes(
+  createTheme(
+    {
+      direction: 'rtl',
+      typography: {
+        fontFamily: 'Estedad',
+      },
+      palette: {
+        background: {
+          // default: "#fafafa",
+        },
       },
     },
-  },
-  faIR,
-  dataGridFaIR
+    faIR,
+    dataGridFaIR
+  )
 )
 
 export const cacheRtl = createCache({
